29971d9935987c20c60309f519447b217bb4cab2
mymindstorm <mymindstorm1@gmail.com>
2018-03-09 15:35:20 -0600

    Stop deploying on test branch

--- ci/tag.sh
+++ ci/tag.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-
 # This script is used by travis to auto tag our releases
+
+# Configure git
 git config --global user.email "builds@travis-ci.com"
 git config --global user.name "Travis CI"
 git remote set-url origin git@github.com:Authenticator-Extension/Authenticator.git
@@ -9,6 +10,8 @@ openssl aes-256-cbc -K $encrypted_2b3e3bd93233_key -iv $encrypted_2b3e3bd93233_i
 chmod 600 $TRAVIS_BUILD_DIR/ci/authenticator-build-key
 eval `ssh-agent -s`
 ssh-add $TRAVIS_BUILD_DIR/ci/authenticator-build-key
+
+# Create and push tag
 git checkout $TRAVIS_BRANCH
 git tag $GIT_TAG -a -m "Automatic tag from TravisCI build $TRAVIS_BUILD_NUMBER"
 git push origin $GIT_TAG
