738a25dda80d51f0a538d9083a2b5d54d929547a
mymindstorm <mymindstorm1@gmail.com>
2018-03-09 15:29:59 -0600

    use ssh

--- ci/tag.sh
+++ ci/tag.sh
@@ -3,6 +3,7 @@
 # This script is used by travis to auto tag our releases
 git config --global user.email "builds@travis-ci.com"
 git config --global user.name "Travis CI"
+git remote set-url origin git@github.com:Authenticator-Extension/Authenticator.git
 export GIT_TAG=v$(grep -m 1 "\"version\"" $TRAVIS_BUILD_DIR/manifest-chrome.json | sed -r 's/^ *//;s/.*: *"//;s/",?//')
 openssl aes-256-cbc -K $encrypted_2b3e3bd93233_key -iv $encrypted_2b3e3bd93233_iv -in $TRAVIS_BUILD_DIR/ci/authenticator-build-key.enc -out $TRAVIS_BUILD_DIR/ci/authenticator-build-key -d
 chmod 600 $TRAVIS_BUILD_DIR/ci/authenticator-build-key
@@ -10,6 +11,4 @@ eval `ssh-agent -s`
 ssh-add $TRAVIS_BUILD_DIR/ci/authenticator-build-key
 git checkout $TRAVIS_BRANCH
 git tag $GIT_TAG -a -m "Automatic tag from TravisCI build $TRAVIS_BUILD_NUMBER"
-git tag
-git show $GIT_TAG
 git push origin $GIT_TAG
