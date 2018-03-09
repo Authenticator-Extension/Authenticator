b741882f4ac0d56b262ae7d65ee658fe3d30b81b
mymindstorm <mymindstorm1@gmail.com>
2018-03-09 14:35:53 -0600

    do directories properly

--- ci/tag.sh
+++ ci/tag.sh
@@ -3,10 +3,10 @@
 # This script is used by travis to auto tag our releases
 git config --global user.email "builds@travis-ci.com"
 git config --global user.name "Travis CI"
-export GIT_TAG=v$(grep -m 1 "\"version\"" manifest-chrome.json | sed -r 's/^ *//;s/.*: *"//;s/",?//')
-openssl aes-256-cbc -K $encrypted_2b3e3bd93233_key -iv $encrypted_2b3e3bd93233_iv -in authenticator-build-key.enc -out authenticator-build-key -d
-chmod 600 ../deploy_key
+export GIT_TAG=v$(grep -m 1 "\"version\"" $TRAVIS_BUILD_DIR/manifest-chrome.json | sed -r 's/^ *//;s/.*: *"//;s/",?//')
+openssl aes-256-cbc -K $encrypted_2b3e3bd93233_key -iv $encrypted_2b3e3bd93233_iv -in $TRAVIS_BUILD_DIR/ci/authenticator-build-key.enc -out $TRAVIS_BUILD_DIR/ci/authenticator-build-key -d
+chmod 600 $TRAVIS_BUILD_DIR/ci/authenticator-build-key
 eval `ssh-agent -s`
-ssh-add authenticator-build-key
+ssh-add $TRAVIS_BUILD_DIR/ci/authenticator-build-key
 git tag $GIT_TAG -a -m "Automatic tag from TravisCI build $TRAVIS_BUILD_NUMBER"
 git push $GIT_TAG
