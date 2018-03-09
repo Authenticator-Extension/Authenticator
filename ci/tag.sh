81a8ce274780a013f222cfbd2a718e8fa833be9a
mymindstorm <mymindstorm1@gmail.com>
2018-03-09 14:29:54 -0600

    authenticate with ssh

--- ci/tag.sh
+++ ci/tag.sh
@@ -4,5 +4,9 @@
 git config --global user.email "builds@travis-ci.com"
 git config --global user.name "Travis CI"
 export GIT_TAG=v$(grep -m 1 "\"version\"" manifest-chrome.json | sed -r 's/^ *//;s/.*: *"//;s/",?//')
+openssl aes-256-cbc -K $encrypted_2b3e3bd93233_key -iv $encrypted_2b3e3bd93233_iv -in authenticator-build-key.enc -out authenticator-build-key -d
+chmod 600 ../deploy_key
+eval `ssh-agent -s`
+ssh-add authenticator-build-key
 git tag $GIT_TAG -a -m "Automatic tag from TravisCI build $TRAVIS_BUILD_NUMBER"
-git push --quiet https://$GITHUBKEY@github.com/Authenticator-Extension/Authenticator $GIT_TAG > /dev/null 2>&1
+git push $GIT_TAG
