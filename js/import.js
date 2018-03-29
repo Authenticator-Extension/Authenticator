24bcb24325c749f91d35d9d8a96432206af21257
Zhe Li <zlhe@microsoft.com>
2018-03-29 15:13:18 +0800

    add lock

--- js/import.js
+++ js/import.js
@@ -1,1 +1,7 @@
 document.title = chrome.i18n.getMessage('import_backup');
+
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+    if (message.action === 'lock') {
+      window.close();
+    }
+  });
