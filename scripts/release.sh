c6c3008354042b987623f7de29fdb2505f168009
Brendan Early <mymindstorm1@gmail.com>
2019-01-30 18:17:29 -0600

    fix script issue

--- scripts/release.sh
+++ scripts/release.sh
@@ -5,9 +5,9 @@
 openssl aes-256-cbc -K $encrypted_cb700307e8da_key -iv $encrypted_cb700307e8da_iv -in scripts/credentials.ts.enc -out src/models/credentials.ts -d
 
 # Build release
-scripts/build.sh chrome
-scripts/build.sh firefox
-scripts/build.sh edge
+bash scripts/build.sh chrome
+bash scripts/build.sh firefox
+bash scripts/build.sh edge
 mkdir release
 mv chrome firefox edge release
 tar -cvzf release.tar.gz release/*
