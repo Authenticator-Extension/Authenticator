dec3b1ee7f1ac395ac0dfdc874b100a892de5e07
Brendan Early <mymindstorm1@gmail.com>
2019-02-20 19:00:08 -0600

    fix #285

--- src/ui/menu.ts
+++ src/ui/menu.ts
@@ -109,6 +109,13 @@ async function menu(_ui: UI) {
         openHelp();
         return;
       },
+      clearFilter: () => {
+        _ui.instance.filter = false;
+        if (_ui.instance.entries.length >= 10) {
+          _ui.instance.showSearch = true;
+        }
+        return;
+      },
       isChrome: () => {
         if (navigator.userAgent.indexOf('Chrome') !== -1) {
           return true;
