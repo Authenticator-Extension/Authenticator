b473117e9aa2805a771545392569172323261416
Brendan Early <mymindstorm@evermiss.net>
2019-07-06 15:26:31 -0500

    sync clock

--- src/components/Popup/MenuPage.vue
+++ src/components/Popup/MenuPage.vue
@@ -88,7 +88,59 @@ export default Vue.extend({
             this.$store.commit('style/showInfo');
             this.$store.commit('currentView/changeView', tab);
             return;
+        },
+        syncClock() {
+            chrome.permissions.request({ origins: ["https://www.google.com/"] }, async granted => {
+                if (granted) {
+                    const message = await syncTimeWithGoogle();
+                    this.$store.commit("notification/alert", this.i18n[message]);
+                }
+                return;
+            });
+            return;
         }
     }
 })
+
+function syncTimeWithGoogle() {
+  return new Promise(
+    (resolve: (value: string) => void, reject: (reason: Error) => void) => {
+      try {
+        // tslint:disable-next-line:ban-ts-ignore
+        // @ts-ignore
+        const xhr = new XMLHttpRequest({ mozAnon: true });
+        xhr.open('HEAD', 'https://www.google.com/generate_204');
+        const xhrAbort = setTimeout(() => {
+          xhr.abort();
+          return resolve('updateFailure');
+        }, 5000);
+        xhr.onreadystatechange = () => {
+          if (xhr.readyState === 4) {
+            clearTimeout(xhrAbort);
+            const date = xhr.getResponseHeader('date');
+            if (!date) {
+              return resolve('updateFailure');
+            }
+            const serverTime = new Date(date).getTime();
+            const clientTime = new Date().getTime();
+            const offset = Math.round((serverTime - clientTime) / 1000);
+
+            if (Math.abs(offset) <= 300) {
+              // within 5 minutes
+              localStorage.offset = Math.round(
+                (serverTime - clientTime) / 1000
+              );
+              return resolve('updateSuccess');
+            } else {
+              return resolve('clock_too_far_off');
+            }
+          }
+        };
+        xhr.send();
+      } catch (error) {
+        return reject(error);
+      }
+    }
+  );
+}
 </script>
--- src/ui/menu.ts
+++ src/ui/menu.ts
@@ -1,47 +1,3 @@
-import { ManagedStorage } from '../models/storage';
-
-export async function syncTimeWithGoogle() {
-  return new Promise(
-    (resolve: (value: string) => void, reject: (reason: Error) => void) => {
-      try {
-        // tslint:disable-next-line:ban-ts-ignore
-        // @ts-ignore
-        const xhr = new XMLHttpRequest({ mozAnon: true });
-        xhr.open('HEAD', 'https://www.google.com/generate_204');
-        const xhrAbort = setTimeout(() => {
-          xhr.abort();
-          return resolve('updateFailure');
-        }, 5000);
-        xhr.onreadystatechange = () => {
-          if (xhr.readyState === 4) {
-            clearTimeout(xhrAbort);
-            const date = xhr.getResponseHeader('date');
-            if (!date) {
-              return resolve('updateFailure');
-            }
-            const serverTime = new Date(date).getTime();
-            const clientTime = new Date().getTime();
-            const offset = Math.round((serverTime - clientTime) / 1000);
-
-            if (Math.abs(offset) <= 300) {
-              // within 5 minutes
-              localStorage.offset = Math.round(
-                (serverTime - clientTime) / 1000
-              );
-              return resolve('updateSuccess');
-            } else {
-              return resolve('clock_too_far_off');
-            }
-          }
-        };
-        xhr.send();
-      } catch (error) {
-        return reject(error);
-      }
-    }
-  );
-}
-
 export async function menu() {
   const ui = {
     data: {},
@@ -56,19 +12,6 @@ export async function menu() {
     //     } else {
     //       return false;
     //     }
-    //   },
-    //   syncClock: async () => {
-    //       chrome.permissions.request(
-    //         { origins: ['https://www.google.com/'] },
-    //         async granted => {
-    //           if (granted) {
-    //             const message = await syncTimeWithGoogle();
-    //             _ui.instance.alert(_ui.instance.i18n[message]);
-    //           }
-    //           return;
-    //         }
-    //       );
-    //     return;
     //   },
     //   migrateStorage: async () => {
     //     // sync => local
