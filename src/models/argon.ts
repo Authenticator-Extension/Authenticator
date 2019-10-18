b7fa05f7ed27745f9cf29f79c29d74e57adfed1f
Brendan Early <mymindstorm@evermiss.net>
2019-10-17 19:21:06 -0500

    update argon2-browser
    up the hasing memory to 16

--- src/models/argon.ts
+++ src/models/argon.ts
@@ -6,7 +6,7 @@ export class argon {
     const hash = await argon2.hash({
       pass: value,
       salt,
-      mem: 1024 * 8,
+      mem: 1024 * 16,
       type: argon2.ArgonType.Argon2id
     });
 
