0d52f2430a4136669ce8290964ebeb9e2dd99f24
Brendan Early <mymindstorm1@gmail.com>
2020-02-09 13:56:45 -0600

    update creds file and credregex

--- src/models/credentials.ts
+++ src/models/credentials.ts
@@ -1,11 +1,16 @@
 export function getCredentials() {
   return {
     drive: {
-      client_id: "", // Google client ID
+      client_id:
+        "", // Google client ID
       client_secret: "" // Google client secret
     },
     dropbox: {
       client_id: "" // Dropbox client ID
+    },
+    onedrive: {
+      client_id: "", // Microsoft Identity client ID
+      client_secret: "" // Microsoft Identity client secret
     }
   };
 }
