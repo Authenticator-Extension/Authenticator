bcf83f23376c83d8ef3f009d172b198877f3dc8b
Brendan Early <mymindstorm@evermiss.net>
2020-03-25 16:02:02 -0500

    fix release script

--- scripts/release.sh
+++ scripts/release.sh
@@ -2,7 +2,7 @@
 # This script builds for release and puts api secrets in relevant files
 
 # Insert secrets
-openssl aes-256-cbc -K $encrypted_cb700307e8da_key -iv $encrypted_cb700307e8da_iv -in credentials.ts.enc -out src/models/credentials.ts -d
+openssl aes-256-cbc -K $encrypted_cb700307e8da_key -iv $encrypted_cb700307e8da_iv -in scripts/credentials.ts.enc -out src/models/credentials.ts -d
 
 # Build release
 bash scripts/build.sh prod
