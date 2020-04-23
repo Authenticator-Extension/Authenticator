4ce9566bfb1aefed39d96234851aa22f402f92a1
Brendan Early <mymindstorm@evermiss.net>
2020-04-23 12:23:29 -0500

    add reset option

---
+++ src/components/Options.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <h2>{{ i18n.delete_all }}</h2>
+    <p>{{ i18n.delete_all_warning }}</p>
+    <input type="checkbox" id="checkbox" v-model="deleteConfirm" />
+    <label for="checkbox">{{ i18n.confirm_delete_all }}</label>
+    <br />
+    <br />
+    <button v-on:click="deleteEverything()" v-bind:disabled="!deleteConfirm">
+      {{ i18n.delete_all }}
+    </button>
+    <br />
+    <p v-show="deleteComplete">{{ i18n.updateSuccess }}</p>
+  </div>
+</template>
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  data: function() {
+    return {
+      deleteConfirm: false,
+      deleteComplete: false
+    };
+  },
+  methods: {
+    async deleteEverything() {
+      await new Promise((resolve: () => void) =>
+        chrome.storage.sync.clear(() => resolve())
+      );
+      await new Promise((resolve: () => void) =>
+        chrome.storage.local.clear(() => resolve())
+      );
+      localStorage.clear();
+      chrome.runtime.sendMessage({ action: "lock" });
+      this.deleteConfirm = false;
+      this.deleteComplete = true;
+    }
+  }
+});
+</script>
---
+++ src/options.ts
@@ -0,0 +1,14 @@
+import Vue from "vue";
+import OptionsView from "./components/Options.vue";
+import { loadI18nMessages } from "./store/i18n";
+
+async function init() {
+  // i18n
+  Vue.prototype.i18n = await loadI18nMessages();
+
+  new Vue({
+    render: h => h(OptionsView)
+  }).$mount("#options");
+}
+
+init();
