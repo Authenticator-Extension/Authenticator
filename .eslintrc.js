d96dafa39ed2188c8155a46794b360775d026e0a
sneezrylee <lizhe@lizhe.org>
2021-02-13 04:20:30 +0800

    update lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,8 @@ module.exports = {
   root: true,
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    "ecmaVersion": 6
+    "ecmaVersion": 6,
+    "sourceType": "module"
   },
   plugins: [
     '@typescript-eslint',
