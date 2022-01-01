c850bfb92514c9c6e4480ec2fcbeb402dab210fc
rebornix <penn.lv@gmail.com>
2022-01-01 15:59:52 -0800

    isEdge.

--- src/browser.ts
+++ src/browser.ts
@@ -2,7 +2,7 @@ const userAgent = navigator.userAgent;
 
 export const isFirefox = (userAgent.indexOf('Firefox') >= 0);
 export const isWebKit = (userAgent.indexOf('AppleWebKit') >= 0);
-export const isEdge = navigator.userAgent.indexOf("Edg") !== -1);
+export const isEdge = navigator.userAgent.indexOf("Edg") >= 0;
 export const isChromium = (userAgent.indexOf('Chrome') >= 0);
 export const isSafari = (!isChromium && (userAgent.indexOf('Safari') >= 0));
 export const isChrome = navigator.userAgent.indexOf("Chrome") !== -1 && navigator.userAgent.indexOf("Edg") === -1;
