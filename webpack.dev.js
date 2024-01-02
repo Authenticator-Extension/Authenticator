5857a9c4067db5eb26bbf077a3ce47676ab0f84d
Brendan Early <mymindstorm@evermiss.net>
2024-01-01 22:53:16 -0600

    update test runner and coverage for mv3

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,17 +1,28 @@
 const path = require("path");
 const { merge } = require('webpack-merge');
 const common = require('./webpack.config.js');
+const { ProvidePlugin } = require("webpack");
 
 module.exports = merge(common, {
   entry: {
     test: "./src/test.ts",
   },
+  // Polyfills for mocha and sinon
   resolve: {
     fallback: {
-      util: require.resolve("util/"),
-      stream: false 
+      "util": require.resolve("util/"),
+      "buffer": require.resolve('buffer/'),
+      "stream": require.resolve("stream-browserify")
     }
   },
+  plugins: [
+    new ProvidePlugin({
+      process: 'process/browser.js',
+    }),
+    new ProvidePlugin({
+      Buffer: ['buffer', 'Buffer'],
+    }),
+  ],
   module: {
     rules: [
       {
@@ -30,5 +41,5 @@ module.exports = merge(common, {
     ],
     // to suppress mocha warnings
     exprContextCritical: false,
-  }
+  },
 });
