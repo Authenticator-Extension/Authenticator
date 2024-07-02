c4cd375dee3a2c88f468379b16e0c6a200c1c559
hicallmeal <76943372+hicallmeal@users.noreply.github.com>
2024-07-03 00:56:45 +1000

    updated patch with proper fix
    Given the issue derives from the `option`s returning the value as a string by default, the 'number' modifier typecasts the value as a number. 
    
    This then correctly flows on to the script execution
    https://v2.vuejs.org/v2/guide/forms.html

--- src/components/Popup/AddAccountPage.vue
+++ src/components/Popup/AddAccountPage.vue
@@ -33,7 +33,7 @@
         <option :value="OTPAlgorithm.GOST3411_2012_256">GOST 34.11 256</option>
         <option :value="OTPAlgorithm.GOST3411_2012_512">GOST 34.11 512</option>
       </a-select-input>
-      <a-select-input :label="i18n.type" v-model="newAccount.type">
+      <a-select-input :label="i18n.type" v-model.number="newAccount.type">
         <option :value="OTPType.totp">{{ i18n.based_on_time }}</option>
         <option :value="OTPType.hotp">{{ i18n.based_on_counter }}</option>
         <option :value="OTPType.battle">Battle.net</option>
@@ -103,7 +103,7 @@ export default Vue.extend({
       ) {
         type = OTPType.hhex;
       } else {
-        type = parseInt(this.newAccount.type);
+        type = this.newAccount.type;
       }
 
       if (type === OTPType.hhex || type === OTPType.hotp) {
